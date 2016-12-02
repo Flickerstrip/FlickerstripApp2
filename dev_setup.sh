ca802b066e3c297fdbe3f679df09cdb22548a6ba
Julian Hartline <julianh2o@gmail.com>
2016-12-02 12:43:29 -0800

    updating dev setup

--- dev_setup.sh
+++ dev_setup.sh
@@ -2,5 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+rm $DIR/.git/hooks/*
 ln -s $DIR/githooks/* $DIR/.git/hooks/
+rm $DIR/fonts
 ln -s $DIR/android/app/src/main/assets/fonts $DIR/
